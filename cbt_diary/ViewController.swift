b5e93d5ed7d45aaa5194223648fd0e643bef4cb3
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-10-29 09:37:47 +0900

    Initial Commit

---
+++ cbt_diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cbt_diary
+//
+//  Created by 丹羽遼吾 on 2019/10/29.
+//  Copyright © 2019 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

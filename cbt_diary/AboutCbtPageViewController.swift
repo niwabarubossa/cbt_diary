cde907813f182b047d77bb4cfa821da154a437ee
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-10-30 21:04:39 +0900

    stabber import success!!

---
+++ cbt_diary/AboutCbtPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutCbtPageViewController.swift
+//  cbt_diary
+//
+//  Created by 丹羽遼吾 on 2019/10/30.
+//  Copyright © 2019 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class AboutCbtPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ cbt_diary/RootViewController.swift
@@ -0,0 +1,58 @@
+import UIKit
+import ESTabBarController
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTab()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupTab() {
+        
+        // 画像のファイル名を指定してESTabBarControllerを作成する
+        let tabBarController: ESTabBarController! = ESTabBarController(tabIconNames: ["exposure", "exposure", "exposure","exposure","exposure"])
+        
+        // 背景色、選択時の色を設定する
+        tabBarController.selectedColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        tabBarController.buttonsBackgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        tabBarController.selectionIndicatorHeight = 3
+        
+        // 作成したESTabBarControllerを親のViewController（＝self）に追加する
+        addChild(tabBarController)
+        let tabBarView = tabBarController.view!
+        tabBarView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tabBarView)
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tabBarView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tabBarView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tabBarView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tabBarView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            ])
+        tabBarController.didMove(toParent: self)
+        
+        // タブをタップした時に表示するViewControllerを設定する
+        let HomePageViewController = storyboard?.instantiateViewController(withIdentifier: "Home")
+        let CbtSheetViewController = storyboard?.instantiateViewController(withIdentifier: "CbtSheet")
+        let MyPageViewController = storyboard?.instantiateViewController(withIdentifier: "MyPage")
+        let AboutCbtViewController = storyboard?.instantiateViewController(withIdentifier: "AboutCbt")
+        
+        
+        tabBarController.setView(HomePageViewController, at: 0)
+        tabBarController.setView(CbtSheetViewController, at: 1)
+        tabBarController.setView(MyPageViewController, at: 3)
+        tabBarController.setView(AboutCbtViewController, at: 4)
+        
+        // 真ん中のタブはボタンとして扱う
+        tabBarController.highlightButton(at: 2)
+        tabBarController.setAction({
+            // ボタンが押されたらImageViewControllerをモーダルで表示する
+//            let imageViewController = self.storyboard?.instantiateViewController(withIdentifier: "ImageSelect")
+//            self.present(imageViewController!, animated: true, completion: nil)
+            print("clicked")
+        }, at: 2)
+    }
+
+}

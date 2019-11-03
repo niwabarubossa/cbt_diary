86da824029e92e68c676938ef98fcb82e8c4511b
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-11-03 12:28:21 +0900

    pause

--- cbt_diary/RootViewController.swift
+++ cbt_diary/RootViewController.swift
@@ -13,10 +13,6 @@ class RootViewController: UIViewController {
         
         // 画像のファイル名を指定してESTabBarControllerを作成する
         let tabBarController: ESTabBarController! = ESTabBarController(tabIconNames: ["exposure", "exposure", "exposure","exposure","exposure"])
-        
-        // 背景色、選択時の色を設定する
-        tabBarController.selectedColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
-        tabBarController.buttonsBackgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
         tabBarController.selectionIndicatorHeight = 3
         
         // 作成したESTabBarControllerを親のViewController（＝self）に追加する

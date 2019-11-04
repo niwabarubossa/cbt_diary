c1f788375e6273186c7f05b3a3c0e4619efa9243
ogaoga <tsutomu.ogasawara+github@gmail.com>
2019-11-04 15:22:16 +0900

    Added a sample.

--- ogasample/ogasample/FirstViewController.swift
+++ ogasample/ogasample/FirstViewController.swift
@@ -8,83 +8,45 @@
 
 import UIKit
 
+// アイテムの定義
+struct Item {
+    var icon: UIImage
+    var title: String
+    var tags: [String]
+}
+
 class FirstViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
+    
+    // 表示するアイテムのリストを定義。
+    var items: [Item] = [
+        Item(icon: UIImage(named: "choushinki")!, title: "自動思考キャッチトレーニング", tags: ["入門", "入門", "入門"]),
+        Item(icon: UIImage(named: "trap_sample")!, title: "TAP・TRAC法", tags: ["入門", "入門", "入門"]),
+        Item(icon: UIImage(named: "trap_sample")!, title: "エクスポージャー", tags: ["入門", "入門", "入門"])
+    ]
+    
+    // セルの名前。StoryBoard でもこのラベルを Cell に指定する。
+    let itemCellName = "ItemCell"
+  
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1 // セクションの数を指定
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return items.count // アイテムの数を指定
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        // viewDidLoad で登録したセルを、ItemCell として取り出す。
+        let cell = tableView.dequeueReusableCell(withIdentifier: itemCellName, for: indexPath) as! ItemCell
 
-        // Configure the cell...
+        // 表示する情報をセルにセットする。
+        let item = self.items[indexPath.row]
+        cell.title.text = item.title
+        cell.tags.text = item.tags.joined(separator: ", ") // とりあえずカンマ区切りで文字列にしています。
+        cell.icon.image = item.icon
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ ogasample/ogasample/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  ogasample
+//
+//  Created by ogaoga on 2019/11/04.
+//  Copyright © 2019 ogaoga.org. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var tags: UILabel!
+}

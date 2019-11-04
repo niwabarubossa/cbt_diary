ee5b651e5989f786abf1865e85bbf2d96a70225b
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-11-04 12:14:07 +0900

    refactoring

--- cbt_diary/AppDelegate.swift
+++ cbt_diary/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  cbt_diary
-//
-//  Created by 丹羽遼吾 on 2019/10/29.
-//  Copyright © 2019 ryogo.niwa. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -15,15 +7,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         var viewControllers: [UIViewController] = []
         
-        // 1ページ目になるViewController
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let firstVC = storyboard.instantiateViewController(withIdentifier: "Home")
         firstVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 1)
         viewControllers.append(firstVC)
-        
 
         let secondVC = storyboard.instantiateViewController(withIdentifier: "CbtSheet")
         secondVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 2)
@@ -33,12 +22,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         thirdVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 3)
         viewControllers.append(thirdVC)
         
-        
-        // ViewControllerをセット
         let tabBarController = UITabBarController()
         tabBarController.setViewControllers(viewControllers, animated: false)
         
-        // rootViewControllerをUITabBarControllerにする
         window = UIWindow()
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
@@ -46,25 +32,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
 

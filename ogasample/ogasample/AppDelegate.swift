a66835968e084cd0f6d10647d59cf4688c947e31
ogaoga <tsutomu.ogasawara+github@gmail.com>
2019-11-04 13:26:58 +0900

    Added my sample.

---
+++ ogasample/ogasample/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ogasample
+//
+//  Created by ogaoga on 2019/11/04.
+//  Copyright © 2019 ogaoga.org. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+  
+}
---
+++ ogasample/ogasample/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ogasample
+//
+//  Created by ogaoga on 2019/11/04.
+//  Copyright © 2019 ogaoga.org. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

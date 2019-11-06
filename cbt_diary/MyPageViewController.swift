bbccd6dd11d1c528b699875a1dd741d0cb258ba8
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-11-06 12:34:07 +0900

    touch event and margin ok

--- cbt_diary/MyPageViewController.swift
+++ cbt_diary/MyPageViewController.swift
@@ -23,11 +23,31 @@ class MyPageViewController: UIViewController,UITableViewDelegate,UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return 1
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 110
     }
 
+    func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.section)番目のセルがタップされました")
+    }
+    
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let marginView = UIView()
+        marginView.backgroundColor = .clear
+        return marginView
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 15  // お好みで
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return .leastNonzeroMagnitude
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 20
+    }
 }
